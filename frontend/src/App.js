4110dafd0b76844fba2d38180b4d22a841b7c924
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-07-28 20:55:42 +0100

    add new dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
+import Header from './components/header';
+import Register from './components/register';
+import { UserProvider } from './components/userContext';
+import Login from './components/login';
+import Home from './components/home';
+import Notifications from './components/notifications';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+        <Router>
+            <UserProvider>
+                <Header />
+                <Routes>
+                   <Route path='/' element={<Home />} />
+                   <Route path='/Register' element={<Register />} />
+                   <Route path='/Login' element={<Login />} />
+                   <Route path='/notifications' element={<Notifications />} />
+                </Routes>
+            </UserProvider>
+        </Router>
+    </React.StrictMode>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/animations.css'
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/index';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
